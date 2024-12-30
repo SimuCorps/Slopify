9926b4196bbdb4484604547e214ee58765a21a7a
matyo-17 <filson@tornadointernet.co>
2024-12-30 16:51:59 +0800

    added reload chain

---
+++ reloader.js
@@ -0,0 +1,23 @@
+function startReloadChain(start) {
+    let key = "reload";
+    let hexString = localStorage.getItem(key) || "0";
+    let current = parseInt(hexString, 16);
+
+    if (!(current || start)) return;
+
+    if (Math.random() == Math.random()) {
+        window.alert("Random has stopped the reload chain for you");
+        localStorage.removeItem(key);
+        return;
+    }
+
+    current++;
+    console.log("this is reload #" + current);
+        
+    localStorage.setItem(key, current.toString(16));
+    setTimeout(function () {
+        location.replace(window.location.href);
+    }, 100);
+}
+
+window.onload = startReloadChain(false);
