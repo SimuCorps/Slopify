f46bee9808667822f3aaa4e55750864c6370e430
maxxus <66540504+MaxxusX@users.noreply.github.com>
2024-12-30 01:50:51 -0600

    Create worsethemebutton.js

---
+++ static/js/worsethemebutton.js
@@ -0,0 +1,10 @@
+const worseThemeButton = document.getElementById("worseThemeButton");
+      document.addEventListener("DOMContentLoaded", () => {
+        const worseThemeButton = document.getElementById("worseThemeButton");
+        worseThemeButton.addEventListener("click", () => {
+          document.body.classList.toggle("worst-theme");
+          worseThemeButton.textContent = document.body.classList.contains("worst-theme") 
+            ? "Fix The Page Please!!" 
+            : "This can't get any worse, right?";
+        });
+      });
