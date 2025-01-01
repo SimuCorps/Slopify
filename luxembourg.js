3edb6ac39b17f47a73954471ca4f5ac7f9c1d0b0
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2025-01-01 18:05:50 -0500

    Tried to do something but failed. :crying_cat_face:

--- luxembourg.js
+++ luxembourg.js
@@ -1,4 +1,7 @@
 class WhatTheFuck {
+    /**
+     * @returns {string}
+     */
     what() {
         return "What the fuck?"
     }
@@ -23,10 +26,24 @@ function main() {
     const maincontent = getStuff("maincontent")
     const schisse = getStuff("schisse")
 
+    let mainHeader = document.createElement("h1")
+    mainHeader.innerText = "Luxembourg"
+
+    let luxembourg = document.createElement("img")
+    luxembourg.setAttribute("src", "https://en.wikipedia.org/wiki/File:Flag_of_Luxembourg.svg")
+
     creation.onclick = () => {
         console.log("hello")
         maincontent.innerHTML = ""
         schisse.innerHTML = ""
+
+        schisse.appendChild(mainHeader)
+
+        document.onmousedown = (event) => {
+            luxembourg.style.cssText = `transform: translate(${event.x, event.y})`
+            maincontent.appendChild(luxembourg)
+            console.log("Hello?")
+        }
     }
 }
 
