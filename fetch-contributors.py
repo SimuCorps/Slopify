0717e6b55ec90c7671a3074b4ed960a2a6eb4238
LordDeatHunter <desuhansa@gmail.com>
2025-01-11 15:04:15 +0100

    Add option for including contributors in the README

--- fetch-contributors.py
+++ fetch-contributors.py
@@ -1,6 +1,7 @@
+import re
+import sys
 import requests
 import json
-
 
 BASE_API_URL = 'https://api.github.com/repos/DishpitDev/Slopify'
 
@@ -20,10 +21,35 @@ def fetch_contributors():
     return all_contributors
 
 
+def get_rounded_img_url(url, size=64):
+    return f'https://images.weserv.nl/?url={url}?h={size}&w={size}&fit=cover&mask=circle'
+
+
+def create_contributor_badge(contributor):
+    return f'<a href="{contributor["html_url"]}"><img src="{get_rounded_img_url(contributor["avatar_url"])}" alt="{contributor["login"]}"></a>'
+
+
+def modify_readme(readme, contributors):
+    badges = '\n'.join([create_contributor_badge(contributor) for contributor in contributors])
+
+    readme = re.sub(r'<!-- CONTRIBUTORS:START -->.*<!-- CONTRIBUTORS:END -->',
+                    f'<!-- CONTRIBUTORS:START -->\n# Contributors ({len(contributors)})\n\n{badges}\n<!-- CONTRIBUTORS:END -->',
+                    readme, flags=re.DOTALL)
+
+    with open('readme.md', 'w', encoding='utf-8') as f:
+        f.write(readme)
+
+
 def main():
     contributors = fetch_contributors()
     with open('contributors.json', 'w') as f:
         json.dump(contributors, f)
+
+    with open('readme.md', 'r', encoding='utf-8') as f:
+        readme = f.read()
+
+    if '--readme' in sys.argv:
+        modify_readme(readme, contributors)
 
 
 if __name__ == '__main__':
