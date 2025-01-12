916886a1e38ae457bcfc4efce821d6311c2273bf
LordDeatHunter <desuhansa@gmail.com>
2025-01-12 18:58:18 +0100

    Add custom emojis to chat app

--- Slopify/src/components/Message.jsx
+++ Slopify/src/components/Message.jsx
@@ -1,3 +1,5 @@
+import emojis, { EMOJI_REGEX, createEmojiElement } from "../utils/emoji.js";
+
 function sanitizeHTML(message) {
   // purges script tags
   message = message.replace(/<script[^>]*>([\s\S]*?)<\/script>/gi, "");
@@ -17,8 +19,27 @@ function sanitizeHTML(message) {
   return message;
 }
 
+function processEmojis(message) {
+  const emojis = message.match(EMOJI_REGEX);
+
+  if (!emojis) {
+    return message;
+  }
+
+  for (let emoji of emojis) {
+    const emojiElement = createEmojiElement(emoji.slice(1, -1));
+    if (!emojiElement) {
+      continue;
+    }
+    message = message.replace(emoji, emojiElement);
+  }
+
+  return message;
+}
+
 export default function Message(data) {
   const sanitizedMessage = sanitizeHTML(data.message);
+  const finalMessage = processEmojis(sanitizedMessage);
 
   return (
     <div key={data.id} className="message">
@@ -44,7 +65,7 @@ export default function Message(data) {
             <strong style={{ color: data.color }}>{data.name}</strong>
             <p
               style={{ margin: 0, marginTop: "0.5rem" }}
-              dangerouslySetInnerHTML={{ __html: sanitizedMessage }}
+              dangerouslySetInnerHTML={{ __html: finalMessage }}
             />
           </div>
         </div>
