14a47ca32202d373106dee5f12fe94037904ca6b
Zebulun McNeill <zeb.mcn@gmail.com>
2025-08-25 15:56:55 -0500

    hello, world!

---
+++ src/org/simucorps/slopify/HelloWorld.java
@@ -0,0 +1,19 @@
+package org.simucorps.slopify;
+
+import javax.swing.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Slopify - Hello World");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            JLabel label = new JLabel("Hello, world!", SwingConstants.CENTER);
+            frame.add(label);
+
+            frame.setSize(400, 200);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
+    }
+}
